1c463d416503099ccbadd540f2b9621f73f51015
Pongsakon <charlee2543@hotmail.com>
2025-03-11 16:27:04 +0700

    test:add teat picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-	return <></>;
+	return (
+		<>
+			<img
+				className=" flex justify-center align-top "
+				src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+				alt="is me"
+			/>
+		</>
+	);
 }
 
 export default App;
