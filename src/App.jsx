48122d9554cded6b0ce2c341fffd9c75d268a1ee
Pongsakon <charlee2543@hotmail.com>
2025-03-17 21:52:01 +0700

    style:edit flex blog aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import HeroSection from "./component/HeroSection";
 import BlogProject from "./component/BlogProject";
 function App() {
 	return (
-		<>
+		<div className=" flex flex-col items-center">
 			<NavBar />
 			<HeroSection />
 			<BlogProject />
-		</>
+		</div>
 	);
 }
 
--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -2,7 +2,7 @@ import AboutMe from "./AboutMe";
 
 const HeroSection = () => {
 	return (
-		<main className="max-w-4xl mx-auto py-12 text-center">
+		<main className="max-w-[1440px] mx-[120px] py-12 text-center">
 			{/* <h2 className="text-3xl font-bold">Blog Pongsakon Meichom</h2> */}
 			<AboutMe />
 		</main>
