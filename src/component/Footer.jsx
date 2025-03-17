b4231699a5f29fbe73784c1de6cfe8755f01f78a
Pongsakon <charlee2543@hotmail.com>
2025-03-18 01:25:23 +0700

    style:reponsive to mobile

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,8 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 const Footer = () => {
 	return (
-		<div className=" px-30 w-full h-[144px] bg-[#efeeeb] flex  justify-between items-center">
-			<div className="flex items-center gap-4">
+		<div className=" px-30 w-full h-[144px] bg-[#efeeeb] flex  justify-between items-center  max-md:flex-col   max-md:justify-around">
+			<div className="flex items-center gap-4 ">
 				<h1 className=" text-2xl ">Get n touch</h1>
 				<a href="#">
 					<Linkedin className=" text-[#43403b] w-[30px] h-[30px]" />
--- src/component/common/Button.jsx
+++ src/component/common/Button.jsx
@@ -1,6 +1,9 @@
 const Button = ({ link, nameButton }) => {
 	return (
-		<a href={link} className="px-4 py-2 rounded-full border">
+		<a
+			href={link}
+			className="px-4 py-2 rounded-full  border bg-white  max-md:border-0"
+		>
 			{nameButton}
 		</a>
 	);
